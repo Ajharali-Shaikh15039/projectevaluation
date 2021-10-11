4e44f4a89d9e59f1a3bf023e8bf2d5f4ab1869f4
Ajharali Shaikh <Ajharal15039@mastek.com>
2021-10-11 16:48:13 +0530

    third commit

---
+++ Pizzania/src/main/java/com/mastek/Pizzania/model/Signup.java
@@ -0,0 +1,14 @@
+package com.mastek.Pizzania.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name ="signup")
+public class Signup {
+	@Id
+	@Column(name="pid")
+	private int id;
+}
